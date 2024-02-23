a8cce4b7aca625f2d144b61a66dde00baa375933
mike.haiduk <mike.haiduk@latoken.com>
2024-02-23 04:03:54 +0300

    create store

--- animation.js
+++ animation.js
@@ -18,3 +18,17 @@ document.querySelectorAll(".animation").forEach(function (i) {
         observer.observe(i);
     }
 });
+
+
+/*const elementsToFadeInUpOnScroll = document.querySelectorAll(".fade-up");
+if (elementsToFadeInUpOnScroll) {
+    window.addEventListener("scroll", function (event) {
+        elementsToFadeInUpOnScroll.forEach(function (element) {
+            if (window.scrollY >= (element.offsetTop - window.innerHeight)) {
+                element.classList.add("fade-in-up");
+            } else {
+                element.classList.remove("fade-in-up");
+            }
+        });
+    });
+}*/
